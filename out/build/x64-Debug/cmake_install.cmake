a07ca935e714aa262910b1cd0b8c5e28b35212dc
Sylda <Sylda@alatek.krakow.pl>
2020-05-15 20:34:33 +0200

    layout code added

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/Sylwek/Desktop/LaunchBox/LaunchBox/out/build/x64-Debug/LaunchBox/cmake_install.cmake")
+  include("C:/Users/Sylwek/Desktop/LaunchBox/LaunchBox/out/build/x64-Debug/Framework/cmake_install.cmake")
 
 endif()
 
