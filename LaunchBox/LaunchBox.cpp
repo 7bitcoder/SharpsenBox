f9307d5d759a4664261c8b4065a23fadb4e4e3d4
Sylda <Sylda@alatek.krakow.pl>
2020-05-15 19:24:30 +0200

    initial commit

---
+++ LaunchBox/LaunchBox.cpp
@@ -0,0 +1,12 @@
+﻿// LaunchBox.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "LaunchBox.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ LaunchBox/LaunchBox.h
@@ -0,0 +1,8 @@
+﻿// LaunchBox.h: plik dołączany dla standardowych systemowych plików dołączanych,
+// lub pliki dołączane specyficzne dla projektu.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: W tym miejscu przywołaj dodatkowe nagłówki wymagane przez program.
