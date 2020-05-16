100b046309b6e822ae94755fcb01cfde90d74a73
Sylda <Sylda@alatek.krakow.pl>
2020-05-16 17:46:03 +0200

    initial code commit

--- Framework/main.cpp
+++ Framework/main.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuick>
+#include "backend.h"
 
 
-int main(int argc, char* argv[]) { 
+int main(int argc, char** argv) {
+	QGuiApplication app(argc, argv);
 
-	std::cout << "elo";
+	QQmlApplicationEngine engine;
+	QQmlContext* rootContext = engine.rootContext();
+	rootContext->setContextProperty("WINDOW_WIDTH", 640);
+	rootContext->setContextProperty("WINDOW_HEIGHT", 360);
+
+
+	engine.load(QStringLiteral("LaunchBox.qml"));
+
+	return app.exec();
 }
