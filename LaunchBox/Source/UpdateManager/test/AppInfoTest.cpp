50574b9cb4e991f9a305ac8f97d9663fe56e03da
sylwow <Sylwo1003@gmail.com>
2020-07-20 18:08:37 +0200

    gtest final used as test framework

--- LaunchBox/Source/UpdateManager/test/AppInfoTest.cpp
+++ LaunchBox/Source/UpdateManager/test/AppInfoTest.cpp
@@ -1,3 +1,16 @@
-﻿#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+﻿#include <gtest/gtest.h>
+#include "AppInfoParser.hpp"
+#include "UpdateManager.hpp"
 
+namespace {
+	im::UpdateManager um_;
+	im::AppInfoParser ap_;
+}
+
+TEST(sample_test_case, sample_test) {
+	EXPECT_EQ(1, 1);
+}
+
+TEST(sample_test_case2, sample_test2) {
+	EXPECT_EQ(1, 1);
+}
