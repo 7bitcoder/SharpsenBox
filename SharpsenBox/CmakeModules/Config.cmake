9f586bdd27875c57e5bf9b851210449e71649b69
7bitcoder <sylwester.dawida@gmail.com>
2024-04-20 20:02:25 +0200

    add auto qt deps installer

--- SharpsenBox/CmakeModules/Config.cmake
+++ SharpsenBox/CmakeModules/Config.cmake
@@ -1,23 +1,26 @@
 set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTORCC ON)
+set(CMAKE_AUTOUIC ON)
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin )
-set( CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin )
-set( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib )
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-set( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
 option(AUTO_QT_LIBS "Runs Qt ulility program to automaticly add qt/qml libraries to bin folder" OFF)
 option(BUILD_TESTS "Build tests" OFF)
+option(Qt6_DIR "Qt directory" "")
 
 # set basic platrofm variables
-if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+if (CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(64BIT 1)
-	set(QtLib ${Qt64Bit})
-elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set(QtLib ${Qt64Bit})
+elseif (CMAKE_SIZEOF_VOID_P EQUAL 4)
     set(32BIT 1)
-	set(QtLib ${Qt32Bit})
-endif()
+    set(QtLib ${Qt32Bit})
+endif ()
 
 # set Qt library path
 # read from file Qt paths
@@ -39,44 +42,34 @@ endif()
 #message("Qt library path: ${QtLib}")
 
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-	set(DEBUG 1)
-	set(type debug)
-else()
-	set(RELEASE 1)
-	set(type release)
-endif()
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(DEBUG 1)
+    set(type debug)
+else ()
+    set(RELEASE 1)
+    set(type release)
+endif ()
 
-if(${CMAKE_SYSTEM_NAME} STREQUAL Windows)
-	set(WIN 1)
-	set(platform win)
-elseif(${CMAKE_SYSTEM_NAME} STREQUAL  Linux)
-	set(LINUX 1)
-	set(platform linux)
-else()
-	set(MAC 1)
-	set(platform mac)
-endif()
+if (${CMAKE_SYSTEM_NAME} STREQUAL Windows)
+    set(WIN 1)
+    set(platform win)
+elseif (${CMAKE_SYSTEM_NAME} STREQUAL Linux)
+    set(LINUX 1)
+    set(platform linux)
+else ()
+    set(MAC 1)
+    set(platform mac)
+endif ()
 
 set(UpdaterPath ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/../Updater)
-if(NOT EXISTS ${UpdaterPath})
-	file(MAKE_DIRECTORY ${UpdaterPath})
-endif()
+if (NOT EXISTS ${UpdaterPath})
+    file(MAKE_DIRECTORY ${UpdaterPath})
+endif ()
 
-if(AUTO_QT_LIBS)
-	add_custom_target(deploy ALL
-		WORKING_DIRECTORY ${QtLib}/bin
-		COMMAND ${platform}deployqt --qmldir ${CMAKE_SOURCE_DIR}/Gui --${type} --quick ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
-		COMMAND ${platform}deployqt --qmldir ${CMAKE_SOURCE_DIR}/Gui --${type} --quick ${UpdaterPath}
-		COMMENT "========= Deploying Aplication ========="
-		USES_TERMINAL)
-		add_dependencies(deploy SharpsenBox)
-endif()
-
-set(totalPath  ${CMAKE_SOURCE_DIR}/Config/SharpsenBox)
+set(totalPath ${CMAKE_SOURCE_DIR}/Config/SharpsenBox)
 set(pages ${CMAKE_SOURCE_DIR}/Gui)
-file( COPY  ${totalPath}/SharpsenBoxInfo.json ${totalPath}/Games.json DESTINATION  ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/../Config )
-file( COPY  ${pages}/ExampleOne.qml ${pages}/ExampleTwo.qml DESTINATION  ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/../Pages )
+file(COPY ${totalPath}/SharpsenBoxInfo.json ${totalPath}/Games.json DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/../Config)
+file(COPY ${pages}/ExampleOne.qml ${pages}/ExampleTwo.qml DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/../Pages)
 
 set(SharpsenBoxPath ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/../SharpsenBox)
 
--- SharpsenBox/Source/App/App/main.cpp
+++ SharpsenBox/Source/App/App/main.cpp
@@ -7,18 +7,16 @@
 
 int main(int argc, char** argv)
 {
-	//responsible is more efficient but animations are too fast
 	//QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
 	QGuiApplication app(argc, argv);
 	app.setWindowIcon(QIcon("orbit2.ico"));
 	QQmlApplicationEngine engine;
-	engine.addImportPath(".");
-	engine.addImportPath("qrc:/");
+	// engine.addImportPath("qrc:/");
 
 	sb::Backend backend(&engine);
 	backend.RegisterComponents();
 	backend.Init();
 
-	engine.load("qrc:/SharpsenBox.qml");
+	engine.load("qrc:/qt/qml/gui/Gui/SharpsenBox.qml");
 	return app.exec();
 }
